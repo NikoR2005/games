7ab56a1f1bf866df74e13d1da2276676445ab9aa
nico <nico@gmail.com>
2018-09-18 22:39:08 +0200

    games skeletons

--- test.py
+++ test.py
@@ -19,15 +19,17 @@ DIRECT_DICT = {pg.K_LEFT: (-1, 0),
 
 class Circle(object):
 
-    def __init__(self, canvas, x, y, rad, cercleColor):
+    def __init__(self, canvas, circleColor, x, y, rad, thickness):
         self.canvas = canvas
         self.x = x
         self.y = y
         self.rad = rad
-        self.cercleColor = cercleColor
+        self.circleColor = circleColor
+        self.thickness = thickness
 
-    def drawcircle(canvas, x, y, rad, cercleColor):
-        cercle = canvas.create_oval(x - rad, y - rad, x + rad, y + rad, width=0, fill=cercleColor)
+    def drawcircle(self):
+       # cercle = canvas.create_oval(x - rad, y - rad, x + rad, y + rad, width=0, fill=cercleColor)
+        pg.draw.circle(self.canvas, self.circleColor, (self.x, self.y), self.rad, self.thickness)
 
 
 
@@ -72,13 +74,22 @@ class Bar(object):
         pg.draw.rect(surface, BAR_COLOR, [10, self.from_left, self.width, self.height], 5)
         pg.draw.rect(surface, BAR_COLOR, [570, self.from_left, self.width, self.height], 5)
 
-
+def event_action(evt):
+    if evt.type == pg.KEYUP:
+        if evt.key == pg.K_SPACE:
+            print('up')
+    if evt.type == pg.KEYDOWN:
+        if evt.key == pg.K_SPACE:
+            print('down')
 
 
 def main():
     """
     Prepare our environment, create a display, and start the program.
     """
+
+    pg.init()
+    running = True
     global SKEL_IMAGE
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
@@ -87,6 +98,8 @@ def main():
     bar = Bar(20, 50, 10, pg.KEYUP, pg.KEYDOWN)
     bar1 = Bar(20, 50, 570, pg.K_a, pg.K_q)
     screen = pg.display.get_surface()
+    circle = Circle(screen, (0,0,255), 150, 50, 15,  3)
+    circle.drawcircle()
     bar.draw(screen)
     pg.display.update()
     bar1.draw(screen)
@@ -94,6 +107,22 @@ def main():
     time.sleep(10)
 
 
+    while running:
+
+        for evt in pg.event.get():
+            event_action(evt)
+            if evt.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if evt.type == pg.KEYDOWN:
+                if evt.key == pg.K_ESCAPE:
+                    running = False
+                elif evt.key == pg.K_DOWN:
+                    print('key hit', bar)
+                    y_change = 5
+                elif evt.key == pg.K_UP:
+                    print('key hit', bar)
+                    y_change = -5
     pg.quit()
     sys.exit()
 
