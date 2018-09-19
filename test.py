573e83bfaba47f4b25786493933978c7ebc5ed88
nico <nico@gmail.com>
2018-09-19 22:07:35 +0200

    games skeletons

--- test.py
+++ test.py
@@ -37,13 +37,14 @@ class Circle(object):
 
 class Bar(object):
 
-    def __init__(self, width, height, from_left, key_up, key_down):
+    def __init__(self, x, y, width, height, key_up, key_down):
+        self.x = x
+        self.y = y
         self.width = width
         self.height= height
-        self.from_left = from_left  # the distance from the left side of the screen.
         self.key_up = key_up
         self.key_down = key_down
-        self.animate_fps = 7
+        self.animate_fps = 144
 
 
     def get_event(self, event):
@@ -71,8 +72,12 @@ class Bar(object):
         Draws the player to the target surface.
         """
 
-        pg.draw.rect(surface, BAR_COLOR, [10, self.from_left, self.width, self.height], 5)
-        pg.draw.rect(surface, BAR_COLOR, [570, self.from_left, self.width, self.height], 5)
+        pg.draw.rect(surface, BAR_COLOR, [self.x, self.y, self.width, self.height], 5)
+
+
+    def move(self, y_change, surface):
+        pg.draw.rect(surface, BAR_COLOR, [self.x, self.y + y_change, self.width, self.height], 5)
+        pg.display.update()
 
 def event_action(evt):
     if evt.type == pg.KEYUP:
@@ -87,28 +92,29 @@ def main():
     """
     Prepare our environment, create a display, and start the program.
     """
-
-    pg.init()
     running = True
+    pg.init()
+
+
+
     global SKEL_IMAGE
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
     pg.display.set_caption(CAPTION)
     pg.display.set_mode(SCREEN_SIZE)
-    bar = Bar(20, 50, 10, pg.KEYUP, pg.KEYDOWN)
-    bar1 = Bar(20, 50, 570, pg.K_a, pg.K_q)
+    bar = Bar(10, 10, 20, 50,  pg.KEYUP, pg.KEYDOWN)
+    bar1 = Bar(590, 80, 20, 50, pg.K_a, pg.K_q)
     screen = pg.display.get_surface()
     circle = Circle(screen, (0,0,255), 150, 50, 15,  3)
     circle.drawcircle()
     bar.draw(screen)
-    pg.display.update()
     bar1.draw(screen)
     pg.display.update()
     time.sleep(10)
 
+    y_change = 0
 
     while running:
-
         for evt in pg.event.get():
             event_action(evt)
             if evt.type == pg.QUIT:
@@ -118,11 +124,14 @@ def main():
                 if evt.key == pg.K_ESCAPE:
                     running = False
                 elif evt.key == pg.K_DOWN:
-                    print('key hit', bar)
-                    y_change = 5
+                    print('key hit', Bar)
+                    bar.move(50, screen)
                 elif evt.key == pg.K_UP:
-                    print('key hit', bar)
+                    print('key hit', Bar)
                     y_change = -5
+
+
+
     pg.quit()
     sys.exit()
 
