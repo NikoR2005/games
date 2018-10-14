45b29ea42688fa1bce6d6147d351149b9a75b4b9
nico <nico@gmail.com>
2018-10-14 20:42:54 +0200

    working game ./first version: tennis

--- tennis.py
+++ tennis.py
@@ -1,228 +1,164 @@
-"""
-This script implements a basic sprite that can only move orthogonally.
-Orthogonal-only movement is slightly trickier than 8-direction movement
-because you can't just create a simple movement vector.
-Extra work must be done to make key overlaps execute cleanly.
--Written by Sean J. McKiernan 'Mekire'
-"""
-
 import os
 import sys
-import itertools
+import time
 
 import pygame as pg
 
+
 CAPTION = "4-Direction Movement with Animation"
-SCREEN_SIZE = (500, 500)
+SCREEN_SIZE = (600, 400)
+SCREEN_SIZE_1 = (300, 200)
 
 BACKGROUND_COLOR = pg.Color("slategray")
+BAR_COLOR = pg.Color("red")
 COLOR_KEY = pg.Color("magenta")
 
-DIRECT_DICT = {pg.K_LEFT: (-1, 0),
-               pg.K_RIGHT: (1, 0),
-               pg.K_UP: (0, -1),
-               pg.K_DOWN: (0, 1)}
 
 
-class Player(object):
-    """
-    This class will represent our user controlled character.
-    """
-    SIZE = (50, 50)
+clock = pg.time.Clock()
+pg.font.init() # you have to call this at the start,
+                   # if you want to use this module.
+myfont = pg.font.SysFont('Comic Sans MS', 30)
+reset_key = pg.K_r
 
-    def __init__(self, pos, speed, facing=pg.K_RIGHT):
-        """
-        The pos argument is a tuple for the center of the player (x,y);
-        speed is in pixels/frame; and facing is the Player's starting
-        direction (given as a key-constant).
-        """
-        self.speed = speed
-        self.direction = facing
-        self.old_direction = None  # Player's previous direction every frame.
-        self.direction_stack = []  # Held keys in the order they were pressed.
-        self.redraw = True  # Forces redraw if needed.
-        self.animate_timer = 0.0
-        self.animate_fps = 7
-        self.image = None
-        self.walkframes = None
-        self.walkframe_dict = self.make_frame_dict()
-        self.adjust_images()
-        self.rect = self.image.get_rect(center=pos)
+class Circle(object):
 
-    def make_frame_dict(self):
-        """
-        Create a dictionary of direction keys to frame cycles. We can use
-        transform functions to reduce the size of the sprite sheet needed.
-        """
-        frames = split_sheet(SKEL_IMAGE, Player.SIZE, 4, 1)[0]
-        flips = [pg.transform.flip(frame, True, False) for frame in frames]
-        walk_cycles = {pg.K_LEFT: itertools.cycle(frames[0:2]),
-                       pg.K_RIGHT: itertools.cycle(flips[0:2]),
-                       pg.K_DOWN: itertools.cycle([frames[3], flips[3]]),
-                       pg.K_UP: itertools.cycle([frames[2], flips[2]])}
-        return walk_cycles
+    v_x = 5
+    v_y = 10
 
-    def adjust_images(self, now=0):
-        """
-        Update the sprite's walkframes as the sprite's direction changes.
-        """
-        if self.direction != self.old_direction:
-            self.walkframes = self.walkframe_dict[self.direction]
-            self.old_direction = self.direction
-            self.redraw = True
-        self.make_image(now)
+    def __init__(self, canvas, circleColor, x, y, rad, thickness, bar1, bar2):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.rad = rad
+        self.circleColor = circleColor
+        self.thickness = thickness
+        self.bar1 = bar1
+        self.bar2 = bar2
 
-    def make_image(self, now):
-        """
-        Update the sprite's animation as needed.
-        """
-        elapsed = now - self.animate_timer > 1000.0 / self.animate_fps
-        if self.redraw or (self.direction_stack and elapsed):
-            self.image = next(self.walkframes)
-            self.animate_timer = now
-        self.redraw = False
 
-    def add_direction(self, key):
-        """
-        Add a pressed direction key on the direction stack.
-        """
-        if key in DIRECT_DICT:
-            if key in self.direction_stack:
-                self.direction_stack.remove(key)
-            self.direction_stack.append(key)
-            self.direction = self.direction_stack[-1]
+    def drawcircle(self, screen):
+       # cercle = canvas.create_oval(x - rad, y - rad, x + rad, y + rad, width=0, fill=cercleColor)
+        pg.draw.circle(screen, self.circleColor, (self.x, self.y), self.rad, self.thickness)
+        print(self.x, self.y, self.y + self.rad, SCREEN_SIZE[1])
+        if self.y + self.rad >= SCREEN_SIZE[1] or self.y <= self.rad:
+            self.v_y = -self.v_y
+        if self.x + self.rad >= SCREEN_SIZE[0] or self.x <= self.rad:
+            self.v_x = -self.v_x
 
-    def pop_direction(self, key):
-        """
-        Pop a released key from the direction stack.
-        """
-        if key in DIRECT_DICT:
-            if key in self.direction_stack:
-                self.direction_stack.remove(key)
-            if self.direction_stack:
-                self.direction = self.direction_stack[-1]
+        if(self.x - self.rad <=  self.bar1.x + self.bar1.width and self.y >=  self.bar1.y and self.y <=  self.bar1.y +  self.bar1.height):
+            self.v_x = -self.v_x
+        if(self.x + self.rad >=  self.bar2.x and self.y >=  self.bar2.y and self.y <=  self.bar2.y +  self.bar2.height):
+            self.v_x = -self.v_x
+        if(SCREEN_SIZE[0] - self.x <= self.rad or self.x <= self.rad):
+            self.v_x = 0
+            self.v_y = 0
 
-    def get_event(self, event):
-        """
-        Handle events pertaining to player control.
-        """
-        if event.type == pg.KEYDOWN:
-            self.add_direction(event.key)
-        elif event.type == pg.KEYUP:
-            self.pop_direction(event.key)
 
-    def update(self, now, screen_rect):
-        """
-        Updates our player appropriately every frame.
-        """
-        self.adjust_images(now)
-        if self.direction_stack:
-            direction_vector = DIRECT_DICT[self.direction]
-            self.rect.x += self.speed * direction_vector[0]
-            self.rect.y += self.speed * direction_vector[1]
-            self.rect.clamp_ip(screen_rect)
+        self.x += self.v_x
+        self.y += self.v_y
 
-    def draw(self, surface):
+class Bar(object):
+
+    def __init__(self, x, y, width, height, key_up, key_down):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height= height
+        self.key_up = key_up
+        self.key_down = key_down
+        self.animate_fps = 144
+        self.bar_size = (24, 96)
+        self.rect = pg.rect.Rect((self.x, self.y, self.width, self.height))
+
+    def handle_keys(self):
+        key = pg.key.get_pressed()
+
+        if key[self.key_up]:
+            print(self.y)
+            if self.y > 0:
+                self.rect.move_ip(0, -5)
+                self.y -= 5
+        if key[self.key_down]:
+            print(self.y)
+            if self.y < SCREEN_SIZE[1] - self.height:
+                self.rect.move_ip(0, 5)
+                self.y += 5
+
+
+
+    def draw(self, screen):
         """
         Draws the player to the target surface.
         """
-        surface.blit(self.image, self.rect)
+        pg.draw.rect(screen, (0, 0, 128), self.rect)
 
 
-class App(object):
-    """
-    A class to manage our event, game loop, and overall program flow.
-    """
+def text_to_screen(screen, text, x, y, size = 50,
+            color = COLOR_KEY):
+    try:
 
-    def __init__(self):
-        """
-        Get a reference to the display surface; set up required attributes;
-        and create a Player instance.
-        """
-        self.screen = pg.display.get_surface()
-        self.screen_rect = self.screen.get_rect()
-        self.clock = pg.time.Clock()
-        self.fps = 60
-        self.done = False
-        self.keys = pg.key.get_pressed()
-        self.player = Player(self.screen_rect.center, 3)
+        text = str(text)
+        text = myfont.render(text, True, color)
+        screen.blit(text, (x, y))
 
-    def event_loop(self):
-        """
-        Pass events on to the player.
-        """
-        for event in pg.event.get():
-            if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
-                self.done = True
-            elif event.type in (pg.KEYUP, pg.KEYDOWN):
-                self.keys = pg.key.get_pressed()
-            self.player.get_event(event)
-
-    def display_fps(self):
-        """
-        Show the program's FPS in the window handle.
-        """
-        caption = "{} - FPS: {:.2f}".format(CAPTION, self.clock.get_fps())
-        pg.display.set_caption(caption)
-
-    def update(self):
-        """
-        Update the player.
-        The current time is passed for purposes of animation.
-        """
-        now = pg.time.get_ticks()
-        self.player.update(now, self.screen_rect)
-
-    def render(self):
-        """
-        Perform all necessary drawing and update the screen.
-        """
-        self.screen.fill(BACKGROUND_COLOR)
-        self.player.draw(self.screen)
-        pg.display.update()
-
-    def main_loop(self):
-        """
-        Our main game loop; I bet you'd never have guessed.
-        """
-        while not self.done:
-            self.event_loop()
-            self.update()
-            self.render()
-            self.clock.tick(self.fps)
-            self.display_fps()
-
-
-def split_sheet(sheet, size, columns, rows):
-    """
-    Divide a loaded sprite sheet into subsurfaces.
-
-    The argument size is the width and height of each frame (w,h)
-    columns and rows are the integer number of cells horizontally and
-    vertically.
-    """
-    subsurfaces = []
-    for y in range(rows):
-        row = []
-        for x in range(columns):
-            rect = pg.Rect((x * size[0], y * size[1]), size)
-            row.append(sheet.subsurface(rect))
-        subsurfaces.append(row)
-    return subsurfaces
-
+    except:
+        print('Font Error, saw it coming')
 
 def main():
     """
     Prepare our environment, create a display, and start the program.
     """
-    global SKEL_IMAGE
-    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    running = True
     pg.init()
+
+
+
     pg.display.set_caption(CAPTION)
     pg.display.set_mode(SCREEN_SIZE)
-    SKEL_IMAGE = pg.image.load("skelly.png").convert()
-    SKEL_IMAGE.set_colorkey(COLOR_KEY)
-    App().main_loop()
+
+    bar1 = Bar(10, 10, 20, 150, pg.K_a, pg.K_q)
+    bar2 = Bar(570, 80, 20, 150, pg.K_UP, pg.K_DOWN)
+    screen = pg.display.get_surface()
+    circle = Circle(screen, COLOR_KEY, 150, 50, 15,  3, bar1, bar2)
+
+    pg.font.init()  # you have to call this at the start,
+    # if you want to use this module.
+    myfont = pg.font.SysFont('Comic Sans MS', 30)
+
+#    screen.blit(bar.box_surface, (20, 10))
+
+    pg.display.update()
+
+
+    y_change = 0
+
+    while running:
+
+        for evt in pg.event.get():
+            if evt.type == pg.KEYDOWN:
+                if evt.key == pg.K_ESCAPE:
+                    running = False
+                if evt.key == pg.K_r:
+                    print('r pressed')
+                    circle.v_x = 5
+                    circle.v_y = 10
+                    circle.x = 300
+                    circle.y = 200
+
+        screen.fill((255, 255, 255))
+
+        bar1.draw(screen)
+        bar2.draw(screen)
+        circle.drawcircle(screen)
+        bar1.handle_keys()
+        bar2.handle_keys()
+        pg.display.update()
+        pg.display.update()
+        clock.tick(40)
+        text_to_screen(screen, '1|2', 50, 50)
+
+
     pg.quit()
     sys.exit()
 
--- test.py
+++ test.py
@@ -1,140 +1,22 @@
-import os
-import sys
-import time
+import pygame
 
-import pygame as pg
+def text_to_screen(screen, text, x, y, size = 50,
+            color = (200, 000, 000), font_type = 'data/fonts/orecrusherexpand.ttf'):
+    try:
 
-CAPTION = "4-Direction Movement with Animation"
-SCREEN_SIZE = (600, 400)
+        text = str(text)
+        font = pygame.font.Font(font_type, size)
+        text = font.render(text, True, color)
+        screen.blit(text, (x, y))
 
-BACKGROUND_COLOR = pg.Color("slategray")
-BAR_COLOR = pg.Color("red")
-COLOR_KEY = pg.Color("magenta")
-
-
-
-clock = pg.time.Clock()
-
-
-class Circle(object):
-
-    v_x = 5
-    v_y = 10
-
-    def __init__(self, canvas, circleColor, x, y, rad, thickness, bar1, bar2):
-        self.canvas = canvas
-        self.x = x
-        self.y = y
-        self.rad = rad
-        self.circleColor = circleColor
-        self.thickness = thickness
-        self.bar1 = bar1
-        self.bar2 = bar2
-
-
-    def drawcircle(self, screen):
-       # cercle = canvas.create_oval(x - rad, y - rad, x + rad, y + rad, width=0, fill=cercleColor)
-        pg.draw.circle(screen, self.circleColor, (self.x, self.y), self.rad, self.thickness)
-        print(self.x, self.y, self.y + self.rad, SCREEN_SIZE[1])
-        if self.y + self.rad >= SCREEN_SIZE[1] or self.y <= self.rad:
-            self.v_y = -self.v_y
-        if self.x + self.rad >= SCREEN_SIZE[0] or self.x <= self.rad:
-            self.v_x = -self.v_x
-
-        if(self.x - self.rad <=  self.bar1.x + self.bar1.width and self.y >=  self.bar1.y and self.y <=  self.bar1.y +  self.bar1.height):
-            self.v_x = -self.v_x
-        if(self.x + self.rad >=  self.bar2.x and self.y >=  self.bar2.y and self.y <=  self.bar2.y +  self.bar2.height):
-            self.v_x = -self.v_x
+    except Exception, e:
+        print 'Font Error, saw it coming'
+        raise \
 
 
 
-
-        self.x += self.v_x
-        self.y += self.v_y
-
-class Bar(object):
-
-    def __init__(self, x, y, width, height, key_up, key_down):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height= height
-        self.key_up = key_up
-        self.key_down = key_down
-        self.animate_fps = 144
-        self.bar_size = (24, 96)
-        self.rect = pg.rect.Rect((self.x, self.y, self.width, self.height))
-
-    def handle_keys(self):
-        key = pg.key.get_pressed()
-
-        if key[self.key_up]:
-            print(self.y)
-            if self.y > 0:
-                self.rect.move_ip(0, -5)
-                self.y -= 5
-        if key[self.key_down]:
-            print(self.y)
-            if self.y < SCREEN_SIZE[1] - self.height:
-                self.rect.move_ip(0, 5)
-                self.y += 5
+eFunk.text_to_screen(screen, 'Text {0}'.format(score), xpos, ypos)
 
 
 
-    def draw(self, screen):
-        """
-        Draws the player to the target surface.
-        """
-        pg.draw.rect(screen, (0, 0, 128), self.rect)
-
-
-
-
-def main():
-    """
-    Prepare our environment, create a display, and start the program.
-    """
-    running = True
-    pg.init()
-
-    pg.display.set_caption(CAPTION)
-    pg.display.set_mode(SCREEN_SIZE)
-    bar1 = Bar(10, 10, 20, 150, pg.K_a, pg.K_q)
-    bar2 = Bar(570, 80, 20, 150, pg.K_UP, pg.K_DOWN)
-    screen = pg.display.get_surface()
-    circle = Circle(screen, COLOR_KEY, 150, 50, 15,  3, bar1, bar2)
-
-
-
-#    screen.blit(bar.box_surface, (20, 10))
-
-    pg.display.update()
-
-
-    y_change = 0
-
-    while running:
-
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                break
-                running = False
-
-        screen.fill((255, 255, 255))
-
-        bar1.draw(screen)
-        bar2.draw(screen)
-        circle.drawcircle(screen)
-        bar1.handle_keys()
-        bar2.handle_keys()
-        pg.display.update()
-        pg.display.update()
-        clock.tick(40)
-
-
-    pg.quit()
-    sys.exit()
-
-
-if __name__ == "__main__":
-    main()
+Funk.text_to_screen(screen, 'Text', xpos, ypos)
