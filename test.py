456866527426c166b27f8de8228db5d25db06ccf
nico <nico@gmail.com>
2019-01-27 21:27:24 +0800

    working game ./first version: tennis

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ from tkinter import *
 CAPTION = "Tetris"
 SCREEN_SIZE = (1200, 1000)
 score = 0
+
 YELLOW = pg.Color("yellow")
 RED = pg.Color("red")
 BLUE = pg.Color("blue")
@@ -18,12 +19,33 @@ BLACK = pg.Color("black")
 clock = pg.time.Clock()
 pg.font.init()
 
-myfont = pg.font.SysFont('Comic Sans MS', 30)
+
+myfont = pg.font.SysFont('maison Sans MS', 30)
+
+cube = [[[0, 0], [0, 1], [1, 0], [1, 1]],
+       [[0, 0], [0, 1], [1, 0], [1, 1]],
+       [[0, 0], [0, 1], [1, 0], [1, 1]],
+       [[0, 0], [0, 1], [1, 0], [1, 1]]]
+
+line = [[[0, 0], [0, 1], [0, 2], [1, 2]],
+       [[0, 0], [0, 1], [0, 2], [1, 2]],
+       [[0, 0], [0, 1], [0, 2], [1, 2]],
+       [[0, 0], [0, 1], [0, 2], [1, 2]]]
+pyramide = [[[0, 0], [0, 1], [0, 2], [1, 1]],
+       [[0, 0], [1, 0], [2, 0], [1, 1]],
+       [[0, 1], [1, 0], [1, 1], [1, 2]],
+       [[0, 0], [0, 1], [0, 2], [1, 2]]]
+angle = [[[0, 0], [0, 1], [0, 2], [1, 0]],
+             [[0, 0], [1, 0], [2, 0], [2, 1]],
+             [[1, 0], [1, 1], [1, 2], [2, 0]],
+             [[0, 0], [0, 1], [0, 2], [1, 2]]]
+
+shapes = [cube, line, pyramide, angle]
 
 
 class Rectangle(object):
 
-    def __init__(self, x, y, width, height, color=RED):
+    def __init__(self, x, y, width, height, color = RED):
         self.x = x
         self.y = y
         self.width = width
@@ -47,6 +69,28 @@ class Circle(object):
         pg.draw.circle(screen, RED, (self.x, self.y), self.rad, self.thickness)
 
 
+class Shape(object):
+    def __init__(self, states, select_color):
+        self.states = states
+        self.color = select_color
+
+        self.state = states[0]
+
+
+    def rotate(self):
+        pass
+
+    def can_move(self,  fix_states):
+        pass
+
+    def fall(self):
+        return self.state
+
+    def draw(self, init_x, init_y, square_size, screen):
+        for coordinates in self.state:
+            rect = Rectangle(init_x + coordinates[0] * square_size, init_y + coordinates[1] * square_size, square_size, square_size, self.color)
+            rect.draw(screen)
+
 def text_to_screen(screen, text, x, y, size=50, color=(2, 2, 25)):
     try:
 
@@ -57,11 +101,11 @@ def text_to_screen(screen, text, x, y, size=50, color=(2, 2, 25)):
     except:
         print('Font Error, saw it coming')
 
-
-def draw_screen(screen):
-    form1_1 = Rectangle(80, 80, 80, 80, RED)
-    form1_2 = Rectangle(80, 160, 80, 80, YELLOW)
-    form1_3 = Rectangle(80, 240, 80, 80)
+def draw_screen_not_used(screen):
+    cube_shape = Shape(cube, RED)
+    cube_shape.draw(80, 80, 80, screen)
+    line_shape = Shape(line, YELLOW)
+    line_shape.draw(80, 240, 80, screen)
     form2_1 = Rectangle(320, 80, 80, 80)
     form2_2 = Rectangle(320, 160, 80, 80)
     form2_3 = Rectangle(320, 240, 80, 80)
@@ -84,9 +128,6 @@ def draw_screen(screen):
     pg.draw.line(screen, (255, 0, 255), (1060, 40), (1060, 1000))
     pg.draw.line(screen, (0, 0, 255), (600, 0), (600, 1000))
     pg.draw.line(screen, (0, 0, 255), (600, 0), (600, 1000))
-    form1_1.draw(screen)
-    form1_2.draw(screen)
-    form1_3.draw(screen)
     form2_1.draw(screen)
     form2_2.draw(screen)
     form2_3.draw(screen)
@@ -112,8 +153,7 @@ def main():
     dsp = pg.display.set_mode(SCREEN_SIZE)
     pg.font.init()
     basicfont = pygame.font.SysFont(None, 60)
-    text = basicfont.render('         Choose your forms.                     Press SPACE start.', True, (55, 0, 0),
-                            (0, 255, 255))
+    text = basicfont.render('         Choose your forms.                     Press SPACE start.', True, (55, 0, 0), (0, 255, 255))
 
     textrect = text.get_rect()
     screen = pg.display.get_surface()
@@ -126,20 +166,37 @@ def main():
             cell = (740 + i * 40, 40 + k * 40, RED, (k, i))
             row.append(cell)
         grid.append(row)
-    print(grid)
 
-    fall_states = [(0, 5), (0 + 1, 6), (0, 7), (0, 6)]
     fix_states = []
 
-    circles = draw_screen(screen)
+
+    circles = draw_screen_not_used(screen)
     while running:
+        shape = Shape(cube[0], RED)
+        state = shape.fall()
+        for row in grid:
+            for cell in row:
+                if cell[3] in cube or cell[3] in fix_states:
+                    rect = Rectangle(cell[0], cell[1], 40, 40, YELLOW)
+                else:
+                    rect = Rectangle(cell[0], cell[1], 40, 40, cell[2])
+                rect.draw(screen)
+
+        next_states = []
+        for state in cube[0]:
+            state = [state[0] + 1, state[1]]
+            next_states.append(state)
+
+        if state[0] == 15:
+            fix_states.extend(cube[0])
 
         ev = pygame.event.get()
         for event in ev:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 for circle in circles:
-                    if circle.x - circle.rad < pos[0] and pos[0] < circle.x + circle.rad and circle.y - circle.rad < pos[1] and pos[1] < circle.y + circle.rad:
+                    if circle.x - circle.rad < pos[0] and pos[0] < circle.x + \
+                            circle.rad and circle.y - circle.rad < pos[1] and pos[1] < circle.y + circle.rad:
                         pg.draw.circle(screen, YELLOW, (circle.x, circle.y), circle.rad, circle.thickness)
                         if circle.thickness == 10:
                             circle.thickness = 2
@@ -147,27 +204,9 @@ def main():
                             circle.thickness = 10
                         circle.drawcircle(screen)
 
-        for row in grid:
-            for cell in row:
-                if cell[3] in fall_states or cell[3] in fix_states:
-                    rect = Rectangle(cell[0], cell[1], 40, 40, YELLOW)
-                else:
-                    rect = Rectangle(cell[0], cell[1], 40, 40, cell[2])
-                rect.draw(screen)
-
-        next_states = []
-        for state in fall_states:
-
-            state = (state[0] + 1, state[1])
-            next_states.append(state)
-
-
-        fall_states = next_states
-        if state[0] == 15:
-            fix_states.extend(fall_states)
-
         pg.display.update()
-        clock.tick(3)
+        clock.tick(10)
+
 
 if __name__ == "__main__":
     main()
