c6b917d2d3cd9a21eae535518b3a09ec26605bf4
nico <nico@gmail.com>
2019-02-03 15:14:22 +0800

    working game ./first version: tennis

--- test.py
+++ test.py
@@ -2,10 +2,15 @@ import pygame.font
 import pygame as pg
 from turtle import *
 from pygame.locals import *
+import time
 
 CAPTION = "Tetris"
 SCREEN_SIZE = (1200, 1000)
-SQUARE_SIZE = 10
+SQUARE_SIZE = 80
+BOX_COORDS = (740, 40)
+BOX_HEIGHT = 20
+BOX_WIDTH = 8
+
 score = 0
 
 YELLOW = pg.Color("yellow")
@@ -16,23 +21,24 @@ clock = pg.time.Clock()
 pg.font.init()
 myfont = pg.font.SysFont('maison Sans MS', 30)
 
+
 cube = [[[0, 0], [0, 1], [1, 0], [1, 1]],
        [[0, 0], [0, 1], [1, 0], [1, 1]],
        [[0, 0], [0, 1], [1, 0], [1, 1]],
        [[0, 0], [0, 1], [1, 0], [1, 1]]]
 
 line = [[[0, 0], [0, 1], [0, 2]],
-       [[0, 0], [1, 0], [2, 0]],
-       [[0, 0], [0, 1], [0, 2]],
-       [[0, 0], [0, 1], [0, 2]]]
+        [[0, 0], [1, 0], [2, 0]],
+        [[2, 0], [2, 1], [2, 2]],
+        [[0, 2], [1, 2], [2, 2]]]
 pyramide = [[[0, 0], [0, 1], [0, 2], [1, 1]],
             [[0, 0], [1, 0], [2, 0], [1, 1]],
-            [[0, 0], [0, 1], [0, 2], [1, 1]],
-            [[0, 0], [1, 0], [2, 0], [1, 1]]]
+            [[2, 0], [2, 1], [2, 2], [1, 1]],
+            [[0, 2], [1, 2], [2, 2], [1, 1]]]
 angle = [[[0, 0], [0, 1], [0, 2], [1, 0]],
          [[0, 0], [1, 0], [2, 0], [2, 1]],
-         [[1, 0], [1, 1], [1, 2], [2, 0]],
-         [[0, 0], [0, 1], [0, 2], [1, 2]]]
+         [[2, 2], [2, 1], [2, 0], [1, 2]],
+         [[0, 1], [0, 2], [2, 2], [1, 2]]]
 
 shapes = [cube, line, pyramide, angle]
 
@@ -63,33 +69,38 @@ class Circle(object):
 
 
 class Shape(object):
-    def __init__(self, x, y, square_size, states, color):
+    def __init__(self, x, y, square_size, states, fill_color, background_color):
         self.states = states
-        self.select_color = color
+        self.select_color = fill_color
+        self.delete_color = background_color
         self.index = 0
         self.state = states[self.index]
         self.x = x
         self.y = y
         self.square_size = square_size
 
-
-    def rotate(self):
+    def rotate(self, screen):
         if self.index == 3:
             self.index = 0
         else:
             self.index = self.index + 1
         self.state = self.states[self.index]
 
-    def can_move(self,  fix_states):
-        pass
+    def can_move(self, fix_states):
+        return True
 
-    def fall(self):
-        pass
+    def fall(self, fix_states):
+        if self.can_move(fix_states):
+            for cell in self.state:
+                cell[1] = cell[1] + 1
 
-    def draw(self, screen):
+    def draw(self, screen, remove):
+        current_color = self.select_color
+        if remove:
+            current_color = self.delete_color
         for coordinates in self.state:
             x, y = self.x + coordinates[0] * self.square_size, self.y + coordinates[1] * self.square_size
-            rect = Square(x, y, self.square_size, self.select_color)
+            rect = Square(x, y, self.square_size, current_color)
             rect.draw(screen)
 
 
@@ -99,7 +110,6 @@ def text_to_screen(screen, text, x, y, size=50, color=(2, 2, 25)):
         text = str(text)
         text = myfont.render(text, True, color)
         screen.blit(text, (x, y))
-
     except:
         print('Font Error, saw it coming')
 
@@ -123,15 +133,16 @@ def draw_screen_not_used(screen):
     circle4.drawcircle(screen)
     return [circle1, circle2, circle3, circle4]
 
+
 def draw_shapes(screen):
-    cube_shape = Shape(80, 80, SQUARE_SIZE, cube, RED)
-    cube_shape.draw(screen)
-    pyramide_shape = Shape(80, 400, SQUARE_SIZE, pyramide, YELLOW)
-    pyramide_shape.draw(screen)
-    angle_shape = Shape(320, 400, SQUARE_SIZE, angle, BLUE)
-    angle_shape.draw(screen)
-    line_shape = Shape(320, 80, SQUARE_SIZE, line, YELLOW)
-    line_shape.draw(screen)
+    cube_shape = Shape(80, 80, SQUARE_SIZE, cube, RED, BLACK)
+    cube_shape.draw(screen, False)
+    pyramide_shape = Shape(80, 400, SQUARE_SIZE, pyramide, YELLOW, BLACK)
+    pyramide_shape.draw(screen, False)
+    angle_shape = Shape(320, 400, SQUARE_SIZE, angle, BLUE, BLACK)
+    angle_shape.draw(screen, False)
+    line_shape = Shape(320, 80, SQUARE_SIZE, line, YELLOW, BLACK)
+    line_shape.draw(screen, False)
     return [cube_shape, pyramide_shape, angle_shape, line_shape]
 
 
@@ -139,7 +150,7 @@ def main():
     running = True
     pg.init()
     pg.display.set_caption(CAPTION)
-    dsp = pg.display.set_mode(SCREEN_SIZE)
+    pg.display.set_mode(SCREEN_SIZE)
     pg.font.init()
     basicfont = pygame.font.SysFont(None, 60)
     text = basicfont.render('         Choose your forms.                     Press SPACE start.', True, (55, 0, 0), (0, 255, 255))
@@ -147,35 +158,28 @@ def main():
     textrect = text.get_rect()
     screen = pg.display.get_surface()
 
-
     grid = []
-    for k in range(20):
+    for k in range(BOX_HEIGHT):
         row = []
-        for i in range(8):
-            cell = (740 + i * 40, 40 + k * 40, RED, (k, i))
+        for i in range(BOX_WIDTH):
+            cell = (BOX_COORDS[0] + i * BOX_COORDS[1], BOX_COORDS[1] + k * SQUARE_SIZE, RED, (k, i))
             row.append(cell)
         grid.append(row)
 
-    fix_states = []
-
+    fix_states = []v
     circles = draw_screen_not_used(screen)
     shapes = draw_shapes(screen)
 
     while running:
-
+        for cell in grid:
+            sq = Square(cell[0], cell[1], SQUARE_SIZE)
+            sq.draw(screen)
         keys = pygame.key.get_pressed()
         if keys[K_SPACE]:
             for shape in shapes:
-                shape.rotate()
-                shape.draw(screen)
-
-        for row in grid:
-            for cell in row:
-                if cell[3] in cube or cell[3] in fix_states:
-                    rect = Square(cell[0], cell[1], 40, YELLOW)
-                else:
-                    rect = Square(cell[0], cell[1], 40, cell[2])
-                rect.draw(screen)
+                shape.draw(screen, True)
+                shape.rotate(screen)
+                shape.draw(screen, False)
 
         next_states = []
         for state in cube[0]:
@@ -191,8 +195,7 @@ def main():
                 pos = pygame.mouse.get_pos()
 
                 for circle in circles:
-                    if circle.x - circle.rad < pos[0] and pos[0] < circle.x + \
-                            circle.rad and circle.y - circle.rad < pos[1] and pos[1] < circle.y + circle.rad:
+                    if circle.x - circle.rad < pos[0] and pos[0] < circle.x + circle.rad and circle.y - circle.rad < pos[1] and pos[1] < circle.y + circle.rad:
                         pg.draw.circle(screen, YELLOW, (circle.x, circle.y), circle.rad, circle.thickness)
                         if circle.thickness == 10:
                             circle.thickness = 2
