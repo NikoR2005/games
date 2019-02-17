e65035e2844e6393747e8672bab03b0493043754
nico <nico@gmail.com>
2019-02-17 09:59:19 +0800

    working game ./first version: tennis

--- tetris.py
+++ tetris.py
@@ -7,22 +7,14 @@ from turtle import *
 from pygame.locals import *
 from tkinter import *
 
-
-
 CAPTION = "Tetris"
-SCREEN_SIZE = (1200,1000)
+SCREEN_SIZE = (1200, 1000)
 score = 0
-BACKGROUND_COLOR = pg.Color("red")
-YELLOW_COLOR = pg.Color("yellow")
-red = (255, 0, 0)
-rectangle1Color = (225, 0, 200 )
-rectangle2Color = (0, 50, 225)
-rectangle3Color = (225, 225, 225)
-rectangle4Color = (1, 1, 1)
-rectangle5Color = (0, 225, 0)
-rectangle6Color = (125, 125, 125)
 
-
+YELLOW = pg.Color("yellow")
+RED = pg.Color("red")
+BLUE = pg.Color("blue")
+BLACK = pg.Color("black")
 
 clock = pg.time.Clock()
 pg.font.init()
@@ -30,56 +22,18 @@ pg.font.init()
 myfont = pg.font.SysFont('Comic Sans MS', 30)
 
 
-class Rectangle():
+class Rectangle(object):
 
-    def __init__(self ,x ,y, width, height, ):
+    def __init__(self, x, y, width, height, color=RED):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-
-
-        #self.rectangle1Color = rectangle1Color
-        #self.rectangle2Color = rectangle2Color
-
+        self.color = color
         self.rect = pg.rect.Rect((self.x, self.y, self.width, self.height))
 
     def draw(self, screen):
-       pg.draw.rect(screen, (0, 0, 255), self.rect)
-
-    def draw1(self, screen):
-        pg.draw.rect(screen, (0, 255, 0), self.rect)
-
-    def draw2(self, screen):
-        pg.draw.rect(screen, (255, 0, 0), self.rect)
-
-    def draw3(self, screen):
-        pg.draw.rect(screen, YELLOW_COLOR, self.rect)
-    def moveside(self, ):
-        key = pg.key.get_pressed()
-
-        if pg.key.get_pressed():
-            pass
-
-
-
-
-
-
-
-def text_to_screen(screen, text, x, y, size = 50,color = (225, 225, 225)):
-    try:
-
-        text = str(text)
-        text = myfont.render(text, True, color)
-        screen.blit(text, (x, y))
-
-    except:
-        print('Font Error, saw it coming')
-
-
-
-
+        pg.draw.rect(screen, self.color, self.rect)
 
 
 class Circle(object):
@@ -90,35 +44,24 @@ class Circle(object):
         self.circleColor = circleColor
         self.thickness = thickness
 
-
-
-
     def drawcircle(self, screen):
-        pg.draw.circle(screen,BACKGROUND_COLOR, (self.x, self.y), self.rad, self.thickness)
+        pg.draw.circle(screen, RED, (self.x, self.y), self.rad, self.thickness)
 
 
+def text_to_screen(screen, text, x, y, size=50, color=(2, 2, 25)):
+    try:
+
+        text = str(text)
+        text = myfont.render(text, True, color)
+        screen.blit(text, (x, y))
+
+    except:
+        print('Font Error, saw it coming')
 
 
-
-
-
-
-
-
-
-
-
-def main():
-    running = True
-    pg.init()
-
-
-
-    pg.display.set_caption(CAPTION)
-    pg.display.set_mode(SCREEN_SIZE)
-    pg.font.init()
-    form1_1 = Rectangle(80, 80, 80, 80)
-    form1_2 = Rectangle(80, 160, 80, 80)
+def draw_screen(screen):
+    form1_1 = Rectangle(80, 80, 80, 80, RED)
+    form1_2 = Rectangle(80, 160, 80, 80, YELLOW)
     form1_3 = Rectangle(80, 240, 80, 80)
     form2_1 = Rectangle(320, 80, 80, 80)
     form2_2 = Rectangle(320, 160, 80, 80)
@@ -132,108 +75,99 @@ def main():
     form4_2 = Rectangle(400, 400, 80, 80)
     form4_3 = Rectangle(320, 480, 80, 80)
     form4_4 = Rectangle(400, 480, 80, 80)
-    circle1 = Circle(BACKGROUND_COLOR,  65, 200, 10, 2)
-    circle2 = Circle(BACKGROUND_COLOR,  65, 520, 10, 2)
-    circle3 = Circle(BACKGROUND_COLOR,  305, 200, 10, 2)
-    circle4 = Circle(BACKGROUND_COLOR,  305, 520, 10, 2)
+    circle1 = Circle(RED, 65, 200, 10, 10)
+    circle2 = Circle(RED, 65, 520, 10, 10)
+    circle3 = Circle(RED, 305, 200, 10, 10)
+    circle4 = Circle(RED, 305, 520, 10, 10)
+    pg.draw.line(screen, (0, 0, 255), (0, 40), (1200, 40))
+    pg.draw.line(screen, (0, 0, 255), (0, 0), (1200, 0))
+    pg.draw.line(screen, (255, 0, 255), (740, 40), (740, 1000))
+    pg.draw.line(screen, (255, 0, 255), (1060, 40), (1060, 1000))
+    pg.draw.line(screen, (0, 0, 255), (600, 0), (600, 1000))
+    pg.draw.line(screen, (0, 0, 255), (600, 0), (600, 1000))
+    form1_1.draw(screen)
+    form1_2.draw(screen)
+    form1_3.draw(screen)
+    form2_1.draw(screen)
+    form2_2.draw(screen)
+    form2_3.draw(screen)
+    form2_4.draw(screen)
+    form3_1.draw(screen)
+    form3_2.draw(screen)
+    form3_3.draw(screen)
+    form3_4.draw(screen)
+    form4_1.draw(screen)
+    form4_2.draw(screen)
+    form4_3.draw(screen)
+    form4_4.draw(screen)
+    circle1.drawcircle(screen)
+    circle2.drawcircle(screen)
+    circle3.drawcircle(screen)
+    circle4.drawcircle(screen)
+    return [circle1, circle2, circle3, circle4]
 
-    basicfont = pygame.font.SysFont(None , 60)
-    text = basicfont.render('         Choose your forms.                     Press SPACE start.', True, (55, 0, 0), (255, 255, 255))
+def main():
+    running = True
+    pg.init()
+    pg.display.set_caption(CAPTION)
+    pg.font.init()
+    basicfont = pygame.font.SysFont(None, 60)
+    text = basicfont.render('         Choose your forms.                     Press SPACE start.', True, (55, 0, 0), (0, 255, 255))
+
     textrect = text.get_rect()
     screen = pg.display.get_surface()
 
 
+    grid = []
+    for k in range(20):
+        row = []
+        for i in range(8):
+            cell = (740 + i * 40, 40 + k * 40, RED, (k, i))
+            row.append(cell)
+        grid.append(row)
 
+    b = 0
+    fall_states = [[b, 5], [b + 1, 6], [b, 7], [b, 6]]
+    fix_states = []
+
+    circles = draw_screen(screen)
     while running:
 
-        screen.fill((255, 255, 255))
-        screen.blit(text, textrect)
+        for row in grid:
+            for cell in row:
+                if cell[3] in fall_states or cell[3] in fix_states:
+                    rect = Rectangle(cell[0], cell[1], 40, 40, YELLOW)
+                else:
+                    rect = Rectangle(cell[0], cell[1], 40, 40, cell[2])
+                rect.draw(screen)
 
-        pg.draw.line(screen, (0, 0, 255), (0, 40), (1200, 40))
-        pg.draw.line(screen, (0, 0, 255), (0, 0), (1200, 0))
-        pg.draw.line(screen, (255, 0, 255), (740, 40), (740, 1000))
-        pg.draw.line(screen, (255, 0, 255), (1060, 40), (1060, 1000))
-        pg.draw.line(screen, (0, 0, 255), (600, 0), (600, 1000))
-        pg.draw.line(screen, (0, 0, 150), (1060, 40), (1060, 1000))
-        pg.draw.line(screen, (0, 0, 150), (1020, 40), (1020, 1000))
-        pg.draw.line(screen, (0, 0, 150), (980, 40), (980, 1000))
-        pg.draw.line(screen, (0, 0, 150), (940, 40), (940, 1000))
-        pg.draw.line(screen, (0, 0, 150), (900, 40), (900, 1000))
-        pg.draw.line(screen, (0, 0, 150), (860, 40), (860, 1000))
-        pg.draw.line(screen, (0, 0, 150), (820, 40), (820, 1000))
-        pg.draw.line(screen, (0, 0, 150), (780, 40), (780, 1000))
-        form1_1.draw(screen)
-        form1_2.draw(screen)
-        form1_3.draw(screen)
-        form2_1.draw1(screen)
-        form2_2.draw1(screen)
-        form2_3.draw1(screen)
-        form2_4.draw1(screen)
-        form3_1.draw2(screen)
-        form3_2.draw2(screen)
-        form3_3.draw2(screen)
-        form3_4.draw2(screen)
-        form4_1.draw3(screen)
-        form4_2.draw3(screen)
-        form4_3.draw3(screen)
-        form4_4.draw3(screen)
-        circle1.drawcircle(screen)
-        circle2.drawcircle(screen)
-        circle3.drawcircle(screen)
-        circle4.drawcircle(screen)
+        next_states = []
+        for state in fall_states:
+
+            state = (state[0] + 1, state[1])
+            next_states.append(state)
+
+        fall_states = next_states
+        if state[0] == 15:
+            fix_states.extend(fall_states)
+            fall_states = [[b, 5], [b + 1, 6], [b, 7], [b, 6]]
+
 
         ev = pygame.event.get()
-
         for event in ev:
-
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
+                for circle in circles:
+                    if circle.x - circle.rad < pos[0] and pos[0] < circle.x + circle.rad and circle.y - circle.rad < pos[1] and pos[1] < circle.y + circle.rad:
+                        pg.draw.circle(screen, YELLOW, (circle.x, circle.y), circle.rad, circle.thickness)
+                        if circle.thickness == 10:
+                            circle.thickness = 2
+                        else:
+                            circle.thickness = 10
+                        circle.drawcircle(screen)
 
-                if circle1.x-circle1.rad <  pos[0] and pos[0] < circle1.x+circle1.rad \
-                        and  circle1.y-circle1.rad <  pos[1] and pos[1] < circle1.y+circle1.rad:
-
-
-                    if circle1.thickness == 10:
-                        circle1.thickness = 2
-                    else:
-                        circle1.thickness = 10
-
-
-                if circle2.x-circle2.rad <  pos[0] and pos[0] < circle2.x+circle2.rad \
-                        and  circle2.y-circle2.rad <  pos[1] and pos[1] < circle2.y+circle2.rad:
-
-
-                    if circle2.thickness == 10:
-                        circle2.thickness = 2
-                    else:
-                        circle2.thickness = 10
-
-
-
-
-
-                if circle3.x-circle3.rad <  pos[0] and pos[0] < circle3.x+circle3.rad \
-                        and  circle3.y-circle3.rad <  pos[1] and pos[1] < circle3.y+circle3.rad:
-
-
-                    if circle3.thickness == 10:
-                        circle3.thickness = 2
-                    else:
-                        circle3.thickness = 10
-                if circle4.x - circle4.rad < pos[0] and pos[0] < circle4.x + circle4.rad \
-                        and circle4.y - circle4.rad < pos[1] and pos[1] < circle4.y + circle4.rad:
-
-                    if circle4.thickness == 10:
-                        circle4.thickness = 2
-                    else:
-                        circle4.thickness = 10
         pg.display.update()
-        clock.tick(40)
-
-
+        clock.tick(10)
 
 if __name__ == "__main__":
-
     main()
-
-
